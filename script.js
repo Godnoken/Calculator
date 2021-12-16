e2e76ae4391a30bbcb0500a280ae0eab37b6045a
Sebastian Ojefors <godnoken@gmail.com>
2021-12-16 16:03:08 +0100

    Add basic calculating functions

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(2, 5));
+
+function subtract(a, b) {
+    return a - b;
+}
+
+console.log(subtract(5, 4));
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(5, 5));
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(divide(40, 4));
